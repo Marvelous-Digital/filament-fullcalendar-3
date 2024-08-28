331aabb4f2029aef421933fda11a1783cef262e4
WAYOFFprod <raphael@wayoff.ch>
2024-08-28 14:31:47 +0200

    some fix

--- resources/js/filament-fullcalendar.js
+++ resources/js/filament-fullcalendar.js
@@ -166,7 +166,6 @@ export default function fullcalendar({
                 },
                 buttonText: buttonText,
             })
-
             calendar.render()
 
             window.addEventListener(
